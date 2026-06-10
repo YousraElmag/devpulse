b5fef8339aab34dc4b424f2c3a2f5c8914c1816c
YousraElmag <yousra3489@gmail.com>
2026-06-10 22:35:05 +0200

    run express server & connect mongodb

---
+++ server/src/server.ts
@@ -0,0 +1,21 @@
+import express, {Request,Response} from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose  from 'mongoose';
+
+dotenv.config();
+const app=express();
+const PORT=process.env.PORT;
+app.use(cors());
+app.use(express.json());
+mongoose
+.connect(process.env.MONGO_DB as string)
+.then(()=>console.log('mongoo connect'))
+.catch((err)=>console.error(err))
+
+app.get('/',(req:Request,res:Response)=>{
+    res.send('server is running with typescript')
+})
+app.listen(PORT,()=>{
+    console.log(`server is running on ${PORT}`)
+})
