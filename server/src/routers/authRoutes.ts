a81aaf474a79f45dd6f462e35d2c3a0f65dadc95
YousraElmag <yousra3489@gmail.com>
2026-06-12 13:01:23 +0200

    install vite ,set up tailwind & proxy

--- server/src/routers/authRoutes.ts
+++ server/src/routers/authRoutes.ts
@@ -2,6 +2,6 @@ import {Router} from "express";
 import {register,login} from "../controllers/authController";
 const router=Router();
 router.post('/register',register)
-router.get('/login',login)
+router.post('/login',login)
 
 export default router
